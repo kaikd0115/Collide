c21dfb57b1c5184ce7719e83920233b7f5c7b0b0
Kai Kodama <kaikodama0701@gmail.com>
2024-10-23 20:13:24 +0900

    Initial Commit

---
+++ Collide/CollideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollideApp.swift
+//  Collide
+//
+//  Created by Kodama Kai on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CollideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
